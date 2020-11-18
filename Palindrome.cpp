45a7f8cdde4572c088311e0c7962552a8fbc1826
namit <singhnamit83@gmail.com>
2020-11-18 13:26:57 +0530

    initial commit

---
+++ Palindrome.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int d,n,sum=0;
+	printf("Enter any number");
+	scanf("%d",&n);
+	while(n>0)
+	{
+	d=n%10;
+	sum=(sum*10)+d;
+	n=n/10;
+	}
+	if(n==sum)
+	printf("Palendrome Number");
+	else
+	("Not Palendrome Number");	
+}
+
